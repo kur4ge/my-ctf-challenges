fdecfe207c240c25385193911b45eeafcf176a80
Kur4ge <i@kur4ge.com>
2021-12-13 17:51:40 +0800

    删除无用CORS代码

--- ByteCTF2021/A-ginx/backend/internal/server/middleware.go
+++ ByteCTF2021/A-ginx/backend/internal/server/middleware.go
@@ -8,19 +8,4 @@ import (
 
 func BindMiddleware(r *gin.Engine) {
 	r.Use(middleware.URITrimMiddleware)
-	/*
-		r.Use(cors.New(cors.Config{
-			AllowOrigins: []string{"*"},
-			AllowMethods: []string{"POST"},
-
-			AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-			ExposeHeaders:    []string{},
-			AllowCredentials: true,
-			AllowOriginFunc: func(origin string) bool {
-				return true
-			},
-			MaxAge: 12 * time.Hour,
-		}))
-	*/
-	// r.Use()
 }
